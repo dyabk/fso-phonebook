17d3499a562d2f71dd80721163b24c5f776241c1
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-03 23:42:29 -0500

    Implemented a case-insensitive search field

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState( [ { 
-    name: 'Arto Hellas',
-    number: '040-1234567'
-  } ] )
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    { name: 'Martin Fowler', number: '', id: 5 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
 
     const names = persons.map(person => person.name)
@@ -26,16 +31,29 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter))
   
   return (
    <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+          value={filter}
+          onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
          name: <input 
@@ -55,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <Person key={person.name}
           name={person.name}
           number={person.number} />)
