fb55e585f711f83d88329e9aee1ceb1bfa0e16be
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-03 20:02:49 -0500

    Created the event handlers to add a new nameObject, update the array

--- src/App.js
+++ src/App.js
@@ -6,19 +6,42 @@ const App = () => {
   ])
 
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+
+    setNewName('')
+    setPersons(persons.concat(nameObject))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   
   return (
    <div>
-     <h2>Phonebook</h2>
-     <form>
-       <div>
-         name: <input />
-       </div>
-       <div>
-         <button type="submit">add</button>
-       </div>
-     </form>
-     <h2>Numbers</h2>
+      <h2>Phonebook</h2>
+      <div>debug: {newName}</div>
+      <form onSubmit={addName}>
+        <div>
+         name: <input 
+          value={newName} 
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}>{person.name}</li>)}
+      </ul>
    </div>
   )
 }
