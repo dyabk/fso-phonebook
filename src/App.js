c88ff6a1c119a62e402dee5b1fc33c997aaab0e2
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-04 00:10:09 -0500

    Refactored the app by defining the Filter, Person, PersonForm, and Persons components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import Person from './components/Person'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -48,37 +50,17 @@ const App = () => {
   return (
    <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input 
-          value={filter}
-          onChange={handleFilterChange}/>
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-         name: <input 
-          value={newName} 
-          onChange={handleNameChange}
-          />
-        </div>
-        <div>
-         number: <input 
-          value={newNumber} 
-          onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person =>
-          <Person key={person.name}
-          name={person.name}
-          number={person.number} />)
-        }
-      </ul>
+      <Filter handler={handleFilterChange} value={filter} />
+      <h3>add a new</h3>
+      <PersonForm
+        name={newName}
+        number={newNumber}
+        nameHandler={handleNameChange}
+        numberHandler={handleNumberChange}
+        adder={addPerson}
+      />
+      <h3>Numbers</h3>
+        <Persons persons={personsToShow} />
    </div>
   )
 }
