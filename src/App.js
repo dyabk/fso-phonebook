7506419e3b505facbd4092660258376de70e48cb
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-18 02:09:30 -0500

    Simplified index.js. Added the .env file to make React to track changes in the index.js file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-    { name: 'Martin Fowler', number: '', id: 5 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
