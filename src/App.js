aa07123204eb40e36706cc5d14e57316329d4059
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-20 12:28:02 -0500

    Extracted all communications with the backend server to its own module file, persons.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,12 +11,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialNumbers => {
+        setPersons(initialNumbers)
       })
   }, [])
   console.log('render', persons.length, 'persons')
@@ -33,10 +31,10 @@ const App = () => {
 
     names.includes(newName)
       ? alert(`${newName} is already added to phonebook`)
-      : axios
-          .post('http://localhost:3001/persons', newPerson)
-          .then(response => {
-            setPersons(persons.concat(newPerson))
+      : personService
+          .create(newPerson)
+          .then(returnedNumber => {
+            setPersons(persons.concat(returnedNumber))
           })
       
     setNewName('')
