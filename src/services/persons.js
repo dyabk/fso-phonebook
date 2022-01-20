ab2772da3f796a56c2f5b2830b4ea930c7845cab
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-20 08:19:49 -0500

    Created a personService object stored in the services\persons.js

---
+++ src/services/persons.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = ( object ) => {
+    const request = axios.post(baseUrl, object)
+    return request.then(response => response.data)
+}
+
+export default { create, getAll }
