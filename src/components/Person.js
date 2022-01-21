b495a51213c9f9f2fd43fb146e6d53fcddb9e0b9
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-21 00:43:09 -0500

    Added a delete button next to every personal record

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Person = ({name, number}) => 
-    <li>{name} {number}</li>
+    <li>
+        {name} {number} 
+        <button onClick={() => console.log('Marked for deletion')}>delete</button>
+    </li>
 
 export default Person
