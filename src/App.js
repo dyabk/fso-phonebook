a12de9f152e5765638fac43bf2f733cf51b66f1d
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-21 19:23:36 -0500

    Added another piece of the App React state: errorMessage. Added a React component called ErrorMessage. Handled the errors caused by accessing deleted records

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
+import ErrorNotification from './components/ErrorNotification'
 import Notification from './components/Notification'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -10,6 +11,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
@@ -50,11 +52,18 @@ const App = () => {
   }
 
   const deletePerson = (id) => {
-    const name = persons.find(person => person.id === id).name
-    if (window.confirm(`Delete ${name}?`)) {
+    const personToDelete = persons.find(person => person.id === id)
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
       personService
         .remove(id)
         .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+        .catch(error => {
+          setErrorMessage(`Information of ${personToDelete.name} has already been removed from server`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 7000)
           setPersons(persons.filter(person => person.id !== id))
         })
     }
@@ -75,9 +84,16 @@ const App = () => {
               setSuccessMessage(null)
             }, 7000)
           })
+          .catch(error => {
+            setErrorMessage(`Information of ${originalObject.name} has already been removed from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 7000)
+            setPersons(persons.filter(person => person.id !== id))
+          })
+      }
     }
-  }
-
+  
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
@@ -95,6 +111,7 @@ const App = () => {
   return (
    <div>
       <h2>Phonebook</h2>
+      <ErrorNotification message={errorMessage} />
       <Notification message={successMessage} />
       <Filter handler={handleFilterChange} value={filter} />
       <h3>add a new</h3>
